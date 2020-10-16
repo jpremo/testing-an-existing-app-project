375c32d27228c4984b517454f16127dfe204f076
Jacob <premojacob@gmail.com>
2020-10-16 16:56:25 -0400

    Worked on phase 2

--- test/get-body-from-request-spec.js
+++ test/get-body-from-request-spec.js
@@ -10,6 +10,9 @@ describe("The getBodyFromRequest function", () => {
 
   it('returns an empty string for no body', done => {
     const bodyPromise = getBodyFromRequest(fakeReq);
+
+
+
     fakeReq.emit('end');
 
     bodyPromise
@@ -23,6 +26,21 @@ describe("The getBodyFromRequest function", () => {
   });
 
   it('returns the data read from the stream', done => {
-    expect.fail('please write this test');
+    const bodyPromise = getBodyFromRequest(fakeReq);
+    const data1 = "This is some";
+    const data2 = " data from the browser";
+
+    fakeReq.emit("data", data1);
+    fakeReq.emit("data", data2);
+    fakeReq.emit('end');
+
+    bodyPromise
+      .then(body => {
+        if (body === data1 + data2) {
+          done();
+        } else {
+          done(`Failed. Got ${body}`);
+        }
+      });
   });
 });
--- test/get-value-from-body-spec.js
+++ test/get-value-from-body-spec.js
@@ -10,12 +10,12 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of ""
-    expect.fail('please write this test');
+    expect(result).to.be.eql("");
   });
 
   it('returns an empty string for a body without the key', () => {
@@ -26,12 +26,12 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of ""
-    expect.fail('please write this test');
+    expect(result).to.be.eql("");
   });
 
   it('returns the value of the key in a simple body', () => {
@@ -41,27 +41,28 @@ describe("The getValueFromBody function", () => {
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value of "Bess"
-    expect.fail('please write this test');
+    expect(result).to.be.eql("Bess")
   });
 
   it('returns the value of the key in a complex body', () => {
     const body = "name=Bess&age=29&job=Boss";
+    const key = "age"
     // Select one of the keys in the body
 
     // Act
     // Write code to invoke getValueFromBody and collect
     // the result
-
+    let result = getValueFromBody(body, key);
 
     // Assert
     // Replace the fail line with an assertion for the
     // expected value for the key that you selected
-    expect.fail('please write this test');
+    expect(result).to.be.eql("29")
   });
 
   it('decodes the return value of URL encoding', () => {
