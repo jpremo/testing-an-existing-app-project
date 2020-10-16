a08f58a73b4e90e0307f176ced73f7ff7d78c689
Jacob <premojacob@gmail.com>
2020-10-16 17:40:04 -0400

    Worked on saving items

--- test/merge-items-spec.js
+++ test/merge-items-spec.js
@@ -18,14 +18,34 @@ describe("The mergeItems function", () => {
   });
 
   it("should return a single <tr>, four <td>s, and a <form> for one uncompleted item", () => {
-    expect.fail('please write this test');
+    let result = mergeItems(template, [{title: "Item 1", category: "one"}])
+    let tds = result.split("<td>")
+    expect(result).to.contain('<tr>');
+    expect(result).to.contain("</tr>");
+    expect(result).to.contain("<td>");
+    expect(result).to.contain("</td>");
+    expect(result).to.contain("</form>")
+    expect(tds.length-1).to.eql(4);
   });
 
   it("should return a single <tr>, four <td>s, and no <form> for one completed item", () => {
-    expect.fail('please write this test');
+    let result = mergeItems(template, [{title: "Item 1", category: "one", isComplete: true}])
+    let tds = result.split("<td>")
+    expect(result).to.contain('<tr>');
+    expect(result).to.contain("</tr>");
+    expect(result).to.contain("<td>");
+    expect(result).to.contain("</td>");
+    expect(result).to.not.contain("</form>")
+    expect(tds.length-1).to.eql(4);
   });
 
   it("should return three <tr>s for three items", () => {
-    expect.fail('please write this test');
+    let result = mergeItems(template, [{title: "Item 1", category: "one", isComplete: true},
+    {title: "Item 2", category: "one", isComplete: true},
+    {title: "Item 3", category: "one", isComplete: true}])
+    expect(result).to.contain('<tr>');
+    expect(result).to.contain("</tr>");
+    let trs = result.split("<tr>")
+    expect(trs.length-1).to.eql(3);
   });
 });
--- test/save-items-spec.js
+++ test/save-items-spec.js
@@ -2,7 +2,10 @@ const { expect } = require('chai');
 const { saveItems } = require('../save-items');
 describe("The saveItems function", () => {
   it('adds the new item to the list', () => {
-    expect.fail('please write this test');
+    let obj = {title: "Item 1", category: "one", isComplete: true};
+    let result = saveItems([{title: "Item 1", category: "one", isComplete: true}], obj );
+    let expected = result.includes(obj)
+    expect(expected).to.be.true
   });
 
   it('makes sure the result and the original are different', () => {
